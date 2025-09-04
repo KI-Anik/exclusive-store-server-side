3b46585858f765967082764912f8927bf74789c9
KI-Anik <iamkhairul101@gmail.com>
2025-09-04 10:32:52 +0600

    feat: add error handling for unhandled rejections and process signals

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import {Server} from 'http';
 import mongoose from 'mongoose';
 import app from './app';
+import { error } from 'console';
 
 let server : Server;
 
@@ -9,7 +10,7 @@ const startServer = async ()=>{
     try {
         await mongoose.connect(process.env.DB_URL as string)
         console.log('connected to db');
-
+        
         server = app.listen(process.env.PORT, ()=>{
             console.log(`Server running on port ${process.env.PORT}`);
         })
@@ -18,4 +19,47 @@ const startServer = async ()=>{
     }
 }
 
-startServer()
+process.on('unhandledRejection', (err)=>{
+    console.log('unhandle Rejection detected... server shutting down', err);
+
+    if(server){
+        server.close(()=>{
+            process.exit(1)
+        })
+    };
+    process.exit(1)
+});
+process.on('uncaughtException', (err)=> {
+    console.log('uncaughtException detected.... server shutting down', err);
+
+    if(server){
+        server.close(()=>{
+            process.exit(1)
+        })
+    };
+    process.exit(1)
+});
+
+process.on('SIGTERM', (err)=>{
+    console.log('sigterm signal received.....server shutting down');
+
+    if(server){
+        server.close(()=>{
+            process.exit(1)
+        })
+    };
+    process.exit(1)
+});
+
+process.on('SIGINT', (err)=>{
+    console.log('sigint signal received...server shutting down');
+
+    if(server){
+        server.close(()=>{
+            process.exit(1)
+        })
+    };
+    process.exit(1)
+});
+
+startServer();
