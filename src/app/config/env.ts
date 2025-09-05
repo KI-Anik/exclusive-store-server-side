d62d1e60b22758d1dcaba780a5c8d172ce0651cc
KI-Anik <iamkhairul101@gmail.com>
2025-09-05 08:47:05 +0600

    feat: implement authentication functionality with login endpoint and JWT handling

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -6,12 +6,13 @@ interface EnvConfig {
     DB_URL: string,
     BCRYPT_SALT_ROUND: string,
 
-    JWT_ACCESS_SECRET: string
+    JWT_ACCESS_SECRET: string,
+    JWT_ACCESS_EXPIRES: string,
 }
 
 const loadEnvVariables = (): EnvConfig => {
     const requiredEnvVariables: string[] = ["PORT", "DB_URL", "BCRYPT_SALT_ROUND",
-        "JWT_ACCESS_SECRET"
+        "JWT_ACCESS_SECRET", "JWT_ACCESS_EXPIRES"
     ]
 
     requiredEnvVariables.forEach(key => {
@@ -25,6 +26,7 @@ const loadEnvVariables = (): EnvConfig => {
         DB_URL: process.env.DB_URL as string,
         BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND as string,
         JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
+        JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
     }
 }
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { UserRouters } from "../app/modules/user/user.route";
+import { authRouters } from "../app/modules/auth/auth.route";
 
 export const router = Router()
 
@@ -7,6 +8,10 @@ const moduleRoutes = [
     {
         path: '/user',
         route: UserRouters
+    },
+    {
+        path: '/auth',
+        route: authRouters
     }
 ]
 
