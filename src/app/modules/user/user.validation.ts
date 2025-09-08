c334453e2923f234e8e020e955aa4519d2b33778
KI-Anik <iamkhairul101@gmail.com>
2025-09-08 14:47:49 +0600

    feat: logOut"

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -11,8 +11,9 @@ export const createUserZodSchema = z.object({
     password: z
     .string({invalid_type_error: "password must be a string"})
     .min(6, {message: "password must be at least 6 character long"}),
-    // picture: z
-    // .string({invalid_type_error: "picture must be a string"})
-    // .url({message: "picture must be a valid url"})
+    picture: z
+    .string({invalid_type_error: "picture must be a string"})
+    .url({message: "picture must be a valid url"})
+    .optional()
 
 })
