47a04eef70751a595743f916a43a1d907d4b0388
KI-Anik <iamkhairul101@gmail.com>
2025-09-08 15:16:06 +0600

    feat: nodemailer

---
+++ src/app/nodemailer/email.service.ts
@@ -0,0 +1,32 @@
+import nodemailer from 'nodemailer';
+import { envVars } from '../config/env';
+
+export interface MailOptions {
+    email: string;
+    subject: string;
+    html: string;
+}
+
+const transporter = nodemailer.createTransport({
+    host: envVars.SMTP_HOST,
+    port: Number(envVars.SMTP_PORT),
+    secure: Number(envVars.SMTP_PORT) === 465, // true for 465, false for other ports
+    auth: {
+        user: envVars.SMTP_USER,
+        pass: envVars.SMTP_PASSWORD,
+    },
+});
+
+
+export const sendEmail = async (options: MailOptions) => {
+    const mailOptions = {
+        from: `Exclusive Store <${envVars.SMTP_USER}>`, // sender address
+        to: options.email, // list of receivers
+        subject: options.subject,
+        html: options.html,
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Message sent: %s', info.messageId);
+    return info;
+};
